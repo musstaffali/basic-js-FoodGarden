1f9d538373e8f861dd53bdd788a9c70d64946264
musstaffali <mamustafa@gtcc.edu>
2021-10-09 19:48:42 -0400

    2 new gardens

--- js/data.js
+++ js/data.js
@@ -4,10 +4,29 @@ var GardensData = [
         "name": "Community Garden",
         "location": "Brown Recreation Center",
         "address": "301 E. Vandalia Rd. Greensboro NC 27406",
-        "occupation": "choocalte farm",
-        "from": "Michigan",
         "telephone": "(336) 373-2920" ,
         "lat": 36.044659,
         "lng": -79.766235
-    }
+    },
+    {
+        "id": 1,
+        "name": "Community Garden",
+        "location": "Steelman Park",
+        "address": "970 Gregory St., Greensboro, NC 27403",
+        "telephone": "(336) 373-2389" ,
+        "lat": 36.05939,
+        "lng": -79.80661
+        //website
+    },
+    {
+        "id": 2,
+        "name": "Community Garden",
+        "location": "Village at Northside Park",
+        "address": "1714 Kay St., Greensboro, NC 27405",
+        "telephone": "(336) 373-2389" ,
+        "lat": 36.10130,
+        "lng": -79.76785
+    },
+
+
 ]
