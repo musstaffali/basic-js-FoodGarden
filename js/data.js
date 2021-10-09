cbb8fb6684bca86604218110ce64b67995aa1433
musstaffali <mamustafa@gtcc.edu>
2021-10-09 15:33:48 -0400

    first commit

---
+++ js/data.js
@@ -0,0 +1,13 @@
+var personData = [
+    {
+        "id": 0,
+        "name": "Food Garden",
+        "age": 36,
+        "followers": 21,
+        "occupation": "choocalte farm",
+        "from": "Michigan",
+        "meow": "cat",
+        "lat": 36.044659,
+        "lng": -79.766235
+    }
+]
